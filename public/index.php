a744f1ca71b8e2222807bb31992b6e7111832142
kawashima1115 <kd1356618@st.kobedenshi.ac.jp>
2024-04-30 15:08:12 +0900

    課題

--- public/index.php
+++ public/index.php
@@ -1,2 +1,12 @@
-<?php
-phpinfo();
+<!DOCTYPE html> <!-- index.php を完全に差し替え -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="sample1.php">練習コード</a>
+    <a href="hello.php">課題</a>
+</body>
+</html>
